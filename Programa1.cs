f1bcfe39a84d12384ccf08dd05c85fe0e0849100
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-23 21:20:03 -0400

    Add files via upload

---
+++ Programa1.cs
@@ -0,0 +1,15 @@
+﻿int[] numeros = new int[10];
+int posicionMayor = 0;
+int mayor = int.MinValue;
+
+for (int i = 0; i < 10; i++) {
+    Console.Write("Ingrese un numero entero: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    if (numeros[i] > mayor) {
+        mayor = numeros[i];
+        posicionMayor = i;
+    }
+}
+
+Console.WriteLine("El numero mayor es " + mayor + " y se encuentra en la posicion " + (posicionMayor + 1) + " del arreglo.");
