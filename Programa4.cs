8494162257c0b6281ea45eaa571cace5ebe3ca6e
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-24 22:07:27 -0400

    Add files via upload

---
+++ Programa4.cs
@@ -0,0 +1,38 @@
+﻿int[] numeros = new int[10];
+
+        // Leer 10 números enteros
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Ingresa un número: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        int contador = 0;
+        // Recorrer el arreglo de números
+        for (int i = 0; i < 10; i++)
+        {
+            int primerdigito = (int)(numeros[i] / Math.Pow(10, (int)Math.Log10(numeros[i])));
+            if (esprimo(primerdigito))
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("Hay " + contador + " números que comienzan con un dígito primo.");
+    
+
+    static bool esprimo(int n)
+    {
+        if (n < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
