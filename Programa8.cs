44e0a4a11b431f6ab80d2445834cb9f0f9316ebf
Richard Almengo <87624842+rixwell@users.noreply.github.com>
2023-01-24 21:21:08 -0400

    Add files via upload

---
+++ Programa8.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[10];
+        int conteonegativo = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Introduzca un numero entero: ");
+            arr[i] = int.Parse(Console.ReadLine());
+
+            
+            if (arr[i] < 0)
+            {
+                conteonegativo++;
+            }
+        }
+
+
+        Console.WriteLine("Existen " + conteonegativo + " números negativos en la matriz.");
+   
+
+
